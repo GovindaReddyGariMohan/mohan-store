b9daa7647caa74ee034a031125f6fd74863ece39
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2023-12-18 18:25:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
   })
   return (
     <div>
-      {/* <HashRouter> */}
+      <HashRouter basename={'/mohan-store'}>
         <Navbar />
         <div className="App">
           <Router />
         </div>
         <Profile />
         <Footer />
-      {/* </HashRouter> */}
+      </HashRouter>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router, HashRouter} from 'react-router-dom';
+//import { BrowserRouter as Router, HashRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <HashRouter basename={'/mohan-store'}>
-      <App />
-    </HashRouter>
+    <App />
   </Provider>
 
 );
