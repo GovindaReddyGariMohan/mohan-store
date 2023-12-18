d40a3bf72478e642cd8910844a4fb6b25cbb7d6f
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2023-12-18 19:26:46 +0530

    first commit

--- src/constants/shopConstants.js
+++ src/constants/shopConstants.js
@@ -97,23 +97,23 @@ const catagory = {
 
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/2bcf907c-bc21-44cb-9021-7575a963b0ef1595934830671-Content-Accesories-Unisex-True-wirless-lets-get-free.jpg',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/bddd9c73-e4f1-43c0-a073-2ff3c0e376b51595934830554-Content-Accesories-men-Sporty-Watches_.jpg',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/5e68d729-b955-431c-930c-931d8b452d421597840217983-Content-accessories-color-white.png',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/8363b55c-446f-4de2-bc5b-e75fd6fdfb2d1597840217862-Content-accessories-brand-fastrack.png',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/274b3659-420d-43c2-8e4c-17249e6eaf8d1597840218139-Content-accessories-trend-minimalist.png',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 }
 
             ],
@@ -127,23 +127,23 @@ const catagory = {
 
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/8becef46-f822-4874-b92a-a7cc7f58819d1597841103131-Content-sportswear-brand-nike.png',
-                    linkUrl: '#/shop/#/shop/men/tshirt'
+                    linkUrl: '#/shop/men/tshirt'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/7472ab66-adf2-4d1d-9379-4770a73c1efe1597841103172-Content-sportswear-brand-proline.png',
-                    linkUrl: '#/shop/#/shop/men/tshirt'
+                    linkUrl: '#/shop/men/tshirt'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/65bafbc5-f83b-4158-8168-f7553043814c1597841103082-Content-sportswear-brand-Asics.png',
-                    linkUrl: '#/shop/#/shop/men/shoes'
+                    linkUrl: '#/shop/men/shoes'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/4aa58fe6-8c61-4e37-9fa8-436c31aecb211597840566511-Content-sportswear-essentials-activewear.png',
-                    linkUrl: '#/shop/#/shop/men/tshirt'
+                    linkUrl: '#/shop/men/tshirt'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/8/21/eedc6c0c-c28b-4ccb-952f-a302c96c59ba1598030134978-Content-mostselling-Sportswear-Nikerevolution.jpeg',
-                    linkUrl: '#/shop/#/shop/men/shoes'
+                    linkUrl: '#/shop/men/shoes'
                 },
             ],
             grid: "2",
@@ -156,23 +156,23 @@ const catagory = {
 
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/a9f68785-e282-425a-b270-c978c387b0f31597840342635-Content-ethnicwear-color-whites.jpg',
-                    linkUrl: '#/shop/#/shop/men/kurtas'
+                    linkUrl: '#/shop/men/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/0d1e0a28-3088-4719-a692-4cdaa7a33cc71597840342726-Content-ethnicwear-occasion-casuallook.jpg',
-                    linkUrl: '#/shop/#/shop/men/kurtas'
+                    linkUrl: '#/shop/men/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9d248917-d1b0-4910-8de0-4ed7c2b4af8e1595935030939-Content-ethnicwear-trends-printedkurtaset.jpg',
-                    linkUrl: '#/shop/#/shop/men/kurtas'
+                    linkUrl: '#/shop/men/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/83d9ca97-4aa3-46ce-bd28-b135d3b94a021595935030673-Content-ethnicwear-essentials-everydaykurtas.jpg',
-                    linkUrl: '#/shop/#/shop/men/kurtas'
+                    linkUrl: '#/shop/men/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9e4fb95e-6268-49c5-9ed1-e6b1bd4b5efd1595935030880-Content-ethnicwear-trend-fashionmeetcomfort.jpg',
-                    linkUrl: '#/shop/#/shop/men/jeans'
+                    linkUrl: '#/shop/men/jeans'
                 },
 
             ],
@@ -184,23 +184,23 @@ const catagory = {
             images: [
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2021/11/17/480f4bde-1d72-4517-be6d-af2dfef7528e1637149111337-MP-Collective.jpg',
-                    linkUrl: '#/shop/#/shop/men/shirts'
+                    linkUrl: '#/shop/men/shirts'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2021/11/17/cadd8457-b8ec-464c-9bc6-6082a24075591637149111331-MP-OriginalPenguin.jpg',
-                    linkUrl: '#/shop/#/shop/men/tshirt'
+                    linkUrl: '#/shop/men/tshirt'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2021/11/17/e185b9a2-0c12-4379-a85c-a4ceed2fe9931637149111326-MP-D1Milano.jpg',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2021/11/17/ab5c5029-d24f-4789-979c-d4910801ea191637149111321-MP-HUGOBoss.jpg',
-                    linkUrl: '#/shop/#/shop/men/tshirt'
+                    linkUrl: '#/shop/men/tshirt'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2021/11/17/621ce33b-3383-4347-99bf-2038b95f27151637149369429-MP-Ducati.jpg',
-                    linkUrl: '#/shop/#/shop/men/watches'
+                    linkUrl: '#/shop/men/watches'
                 },
 
             ],
@@ -218,14 +218,14 @@ const catagory = {
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/25/c9f66558-feab-4d76-aa3c-adc68d81dce21598348260415-Levis.jpg',
-                    linkUrl: '#/shop/#/shop/men/shirts'
+                    linkUrl: '#/shop/men/shirts'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/5/178c1e5d-69f2-402f-a2a5-ef44700a0f691596640983793-Roadster---.jpg'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/25/085719b1-c71e-4f47-950c-9a6b7f291fac1598348260370-Jack-_-Jones.jpg',
-                    linkUrl: '#/shop/#/shop/men/shirts'
+                    linkUrl: '#/shop/men/shirts'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/5/774f42c4-f459-4514-9b90-cf8a60a5f68c1596644478087-hrx30.jpg'
@@ -281,41 +281,41 @@ const catagory = {
 
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/814b8ff4-1dec-4a6e-86b8-c26f5c40fe4c1598892518923-Biba.jpg',
-                    linkUrl: '#/shop/#/shop/womens/kurtas'
+                    linkUrl: '#/shop/womens/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/e72c82f2-cfd5-4f9a-b1b0-ba9e2b3e51251598892519506-W.jpg',
-                    linkUrl: '#/shop/#/shop/womens/kurtas'
+                    linkUrl: '#/shop/womens/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/48aa9321-1906-4471-be70-20ef124459971598892519217-M_S.jpg',
-                    linkUrl: '#/shop/#/shop/womens/kurtas'
+                    linkUrl: '#/shop/womens/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/64ecc170-58af-473f-8144-b1639895a8291598892519457-Veromodo.jpg',
-                    linkUrl: '#/shop/#/shop/womens/kurtas'
+                    linkUrl: '#/shop/womens/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/3f125d86-5139-4ee2-8561-5a610c2624421598892519314-Only.jpg',
-                    linkUrl: '#/shop/#/shop/womens/kurtas'
+                    linkUrl: '#/shop/womens/kurtas'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/90c35c24-ac69-4472-a1a0-be82c087ec181598892519026-F21.jpg'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/61c793f2-399c-4b8a-9e93-831adef697381598892519164-Levis.jpg',
-                    linkUrl: '#/shop/#/shop/womens/shoes'
+                    linkUrl: '#/shop/womens/shoes'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/8f070770-db34-4e20-ab80-59728106460a1598892519078-GAP.jpg'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/1aa324e4-6230-4cf9-a50c-a37413eab1381598892519266-Nike.jpg',
-                    linkUrl: '#/shop/#/shop/womens/sportswear'
+                    linkUrl: '#/shop/womens/sportswear'
                 },
                 {
                     imgUrl: 'https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/581e2bf6-6d47-4a4b-a11f-43200dc9c6791598892519362-Puma.jpg',
-                    linkUrl: '#/shop/#/shop/womens/shoes'
+                    linkUrl: '#/shop/womens/shoes'
                 },
 
             ],
