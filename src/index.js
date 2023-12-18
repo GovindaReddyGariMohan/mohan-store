b7db96d3b9232c2ea0b4c1e6a5a69ff599b6395c
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2023-12-18 17:00:59 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <BrowserRouter basename={'/mohan-store'}>
+    <HashRouter basename={'/mohan-store'}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 
 );
