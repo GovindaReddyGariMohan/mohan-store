33bcf6541a7972acca7e7f0385d95eb46512e00e
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2023-12-19 09:25:42 +0530

    first commit

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -8,36 +8,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleProfileModal } from "../redux/actions/appActions";
 import { addItem } from "../redux/actions/cartActions";
 const Cart = () => {
-    const { cartData, cartTotal } = useSelector((state) => state.cart)
+    const { cartData } = useSelector((state) => state.cart)
     const dispatch = useDispatch()
     useEffect(() => {
         const items = localStorage.getItem('cart')
-        if (items.length !== null) {
+        if (items !== null) {
             const cartItemIds = items.split(",");
             if (cartItemIds.length > 0) {
                 const filterId = Plpproducts.filter((items) => cartItemIds.some((id) => items.id === id))
                 dispatch(addItem(filterId))
-
             }
         }
-
         dispatch(toggleProfileModal(true))
         return () => dispatch(toggleProfileModal(false))
     }, [])
-    return cartData && (
-        <div className="cart">
-            <div className="row">
-                <div className="col-7">
-                    <Cartleft />
+    const cartItems = localStorage.getItem('cart')
+    if (cartItems === null  ) {
+        return(
+            <div className="cart-items">Cart is Empty Please Add The Items to The Cart</div>
+        )
+    } else {
+        return cartData && (
+            <div className="cart">
+                <div className="row">
+                    <div className="col-7">
+                        <Cartleft />
+                    </div>
+                    <div className="col-5">
+                        <Cartright />
+                    </div>
                 </div>
-                <div className="col-5">
-                    <Cartright />
+                <div>
+                    <Cartbottom />
                 </div>
             </div>
-            <div>
-                <Cartbottom />
-            </div>
-        </div>
-    )
+        )
+    }
 }
 export default Cart;
